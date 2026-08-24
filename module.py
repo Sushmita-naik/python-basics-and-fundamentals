0c4d26aad38130b408af4dfd7e6b16882ebf23f2
Sushmita Naik <sushmitanaik075@gmail.com>
2026-08-24 22:52:01 +0530

    Initial commit

---
+++ module.py
@@ -0,0 +1,9 @@
+# import pyjokes
+# print("Printing jokes..............")
+# joke=pyjokes.get_joke()
+# print(joke)
+
+import pyttsx3
+name=pyttsx3.init()
+name.say("Hi sushmita , how are you! I hope you are good now you are with me.......!")
+name.runAndWait
---
+++ practise.py
@@ -0,0 +1,58 @@
+# just basics
+# a=1
+# b=1
+# print(a+b)
+
+# operators in python
+# a=10
+# b=20
+# c=a+b
+# print(c)
+# print(a*b)
+# print(c//b)
+# print(a-b)
+
+# # comparision operator
+# d=10>5
+# print(d)
+# c= 10==10
+# print(c)
+
+# # logical operators
+# m=True and False
+
+# type casting and type function
+# a=30
+# print(type(a))
+# b=31.4
+# print(type(b))
+# c="Sush"
+# print(type(c))
+# x=float(a)
+# print(x)
+# print(type(x))
+
+# # input function
+# a=int(input("Enter a number:"))
+# b=int(input("Enter another number:"))
+# print("Sum is ", a+b)
+
+# a=int(input("Enter a number 1:"))
+# b=int(input("Enter a number 2:"))
+# print(a>b)
+
+# a=int(input("Enter a number 1:"))
+# b=int(input("Enter a number 2:"))
+# print("Average of 2 numbers are:",(a+b)/2)
+
+# a=int(input("Enter a number 1:"))
+# print("Square of a number",a, "is:",a*a)
+
+# strings
+name="Sushmita"
+print(len(name)) # to find the lenth of string
+print(name[2:4])
+print(name[-4:-1])
+print(name[1:])
+print(name[:8])
+
