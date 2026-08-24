6ea15191c7ffffc39033931dead14fa675646950
Sushmita Naik | Data & Python Enthusiast <sushmitanaik527@gmail.com>
2026-08-24 22:54:26 +0530

    Uncommented code for basic Python operations

--- practise.py
+++ practise.py
@@ -1,52 +1,52 @@
 # just basics
-# a=1
-# b=1
-# print(a+b)
+a=1
+b=1
+print(a+b)
 
 # operators in python
-# a=10
-# b=20
-# c=a+b
-# print(c)
-# print(a*b)
-# print(c//b)
-# print(a-b)
+a=10
+b=20
+c=a+b
+print(c)
+print(a*b)
+print(c//b)
+print(a-b)
 
-# # comparision operator
-# d=10>5
-# print(d)
-# c= 10==10
-# print(c)
+# comparision operator
+d=10>5
+print(d)
+c= 10==10
+print(c)
 
-# # logical operators
-# m=True and False
+# logical operators
+m=True and False
 
 # type casting and type function
-# a=30
-# print(type(a))
-# b=31.4
-# print(type(b))
-# c="Sush"
-# print(type(c))
-# x=float(a)
-# print(x)
-# print(type(x))
+a=30
+print(type(a))
+b=31.4
+print(type(b))
+c="Sush"
+print(type(c))
+x=float(a)
+print(x)
+print(type(x))
 
-# # input function
-# a=int(input("Enter a number:"))
-# b=int(input("Enter another number:"))
-# print("Sum is ", a+b)
+# input function
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+print("Sum is ", a+b)
 
-# a=int(input("Enter a number 1:"))
-# b=int(input("Enter a number 2:"))
-# print(a>b)
+a=int(input("Enter a number 1:"))
+b=int(input("Enter a number 2:"))
+print(a>b)
 
-# a=int(input("Enter a number 1:"))
-# b=int(input("Enter a number 2:"))
-# print("Average of 2 numbers are:",(a+b)/2)
+a=int(input("Enter a number 1:"))
+b=int(input("Enter a number 2:"))
+print("Average of 2 numbers are:",(a+b)/2)
 
-# a=int(input("Enter a number 1:"))
-# print("Square of a number",a, "is:",a*a)
+a=int(input("Enter a number 1:"))
+print("Square of a number",a, "is:",a*a)
 
 # strings
 name="Sushmita"
