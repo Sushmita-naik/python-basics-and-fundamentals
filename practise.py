a2dc4df4e6465ce233186d73072241010b534666
Sushmita Naik <sushmitanaik075@gmail.com>
2026-08-26 23:00:53 +0530

    Updated Basics code

--- practise.py
+++ practise.py
@@ -56,3 +56,72 @@ print(name[-4:-1])
 print(name[1:])
 print(name[:8])
 
+# skipping of the string
+word="Sushmita"
+print(word[1:8:3])
+
+# another types
+print(word[9:])
+print(word[:9])
+print(word[0:9])
+
+string functions
+# to find the length 
+str="Sammu"
+print(len(str))
+print(str.endswith("mmu"))
+print(str.endswith("mmua")) #to find the if string ends with specific characters or not
+print(str.startswith("sa"))#to find the string is start with specific character or not
+print(str.capitalize()) # to make the first word as capital
+index=str.find("mmu")
+print(index) #to find the index of the word
+another="sushmita want "
+replace=another.replace("sushmita","ranju")
+print(replace) # to replace the original string
+
+# problms
+name=input("Ente your name:")
+print(f"Good evening {name}")
+
+letter='''
+Dear <|name|>,
+You are selected!
+<|Date|>
+'''
+print(letter)
+
+replced_string=letter.replace("<|name|>","Sushmita")
+replced_string=letter.replace("<|Date|>","28/10/2028")
+print(replced_string)
+print(letter.replace("<|name|>","Sammu").replace("<|Date|>","28/10/2028")) # if we want to replace in one line of code
+
+name="Sushmita is a good girl"
+print(name.find("is"))
+print(name.replace(" ","     ")) # strings are immutable
+
+sentence="Dear Sushmita,\n\t you are a quick learner,\nthank you"
+print(sentence)
+
+list and tuple
+names=["Sammu","ramju","Sussu","Shara","nishmi"]
+print(names)
+names[1]="Sanika"
+print(names)
+print(names[0:4]) #slicing of list
+
+# methods of string
+names.append("Sanika") # to add the new elem into list
+print(names)
+
+numbers=[23,45,12,34,56,76,43,23]
+numbers.sort() # to sort the given lost
+print(numbers)
+numbers.reverse()
+print(numbers)
+numbers.insert(4,100) # inserting the new value at respected index with new value or elem
+print(numbers)
+numbers.pop(3)
+print(numbers)
+print(numbers.pop(3))
+numbers.remove(56)
+print(numbers)
