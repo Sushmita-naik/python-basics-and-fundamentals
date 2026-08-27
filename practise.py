8a310558d6c9e2a92973fe4555bea46f2dc03d54
Sushmita Naik <sushmitanaik075@gmail.com>
2026-08-27 23:10:22 +0530

    learnt about dictionary and sets and tuple

--- practise.py
+++ practise.py
@@ -126,89 +126,182 @@
 # numbers.remove(56)
 # print(numbers)
 
-# tuple
-a=(1,2,3,4,5)
-print(type(a))
-# to create an empty tuple
-b=()
-print(type(b))
-c=("sushmita",123,456,"Ranjita","Sammu",23.45)
-print(c)
-print(c.count(456))
-print(c.index("sushmita"))
+# # tuple
+# a=(1,2,3,4,5)
+# print(type(a))
+# # to create an empty tuple
+# b=()
+# print(type(b))
+# c=("sushmita",123,456,"Ranjita","Sammu",23.45)
+# print(c)
+# print(c.count(456))
+# print(c.index("sushmita"))
 
-some=(1,2,34,56,78,98,56,34,100)
-print(min(some))
-print(max(some))
-print(len(some))
-print(sum(some))
-print(sorted(some))
-print(tuple(reversed(some)))
-print(some.index(98))
-print(some.count(56))
+# some=(1,2,34,56,78,98,56,34,100)
+# print(min(some))
+# print(max(some))
+# print(len(some))
+# print(sum(some))
+# print(sorted(some))
+# print(tuple(reversed(some)))
+# print(some.index(98))
+# print(some.count(56))
 
-# some other tuple operations
-# slicing of tuple
-t1=("Sushmita","Ranjita","Sammu","Sanika","Nishmi")
-print(t1[0:4])
-t2=("Sharanya","Sharadvi","Prakrithi","Shobha")
-T=t1+t2 # concatination of tuple
-print(f"a new tuple is {T}")
+# # some other tuple operations
+# # slicing of tuple
+# t1=("Sushmita","Ranjita","Sammu","Sanika","Nishmi")
+# print(t1[0:4])
+# t2=("Sharanya","Sharadvi","Prakrithi","Shobha")
+# T=t1+t2 # concatination of tuple
+# print(f"a new tuple is {T}")
 
-# repetation or multiplication of a tuple
-tup1=(112,34,56,7,83,4,5)
-print(tup1 * 2)
-print(tup1 * 3 )
+# # repetation or multiplication of a tuple
+# tup1=(112,34,56,7,83,4,5)
+# print(tup1 * 2)
+# print(tup1 * 3 )
 
-# to find if the elem is present or not in the tuple then we use membership operation
-tup2=(1010,2,3,45,6,7,8,56,78)
-print(45 in tup2)
-print(100 in tup2)
+# # to find if the elem is present or not in the tuple then we use membership operation
+# tup2=(1010,2,3,45,6,7,8,56,78)
+# print(45 in tup2)
+# print(100 in tup2)
 
-# to compare the 2 tuple then we use comparision operation in tuple
-tup3=(101,230,45)
-tup4=(101,230,45)
-tup5=(102,130,56)
-print(tup3==tup4)
-print(tup4==tup5)
+# # to compare the 2 tuple then we use comparision operation in tuple
+# tup3=(101,230,45)
+# tup4=(101,230,45)
+# tup5=(102,130,56)
+# print(tup3==tup4)
+# print(tup4==tup5)
 
-# iteration operator
-tup6=(23,45,67,89,90)
-for i in tup6:
-    print(i)
+# # iteration operator
+# tup6=(23,45,67,89,90)
+# for i in tup6:
+#     print(i)
 
-# removing and adding the tuples values to anothe variable use packaging and unpackaging (to add the value in unpacking variable)
-t=(19,68,78,90)
-x,y,z,l=t
-print(x,y,z,l)
+# # removing and adding the tuples values to anothe variable use packaging and unpackaging (to add the value in unpacking variable)
+# t=(19,68,78,90)
+# x,y,z,l=t
+# print(x,y,z,l)
+
+# # problems
+# fruits=[]
+# f1=input("Enter one fruit:")
+# f2=input("Enter another fruit:")
+# f3=input("Enter next fruit:")
+# f4=input("Enter next fruit:")
+# fruits.append(f1)
+# fruits.append(f2)
+# fruits.append(f3)
+# fruits.append(f4)
+# print(f"The final list that has fruits is {fruits}")
+
+# marks=[]
+# m1=float(input("Enter first student marks:"))
+# m2=float(input("Enter second student marks:"))
+# m3=float(input("Enter third student marks:"))
+# m4=float(input("Enter fourth student marks:"))
+# m5=float(input("Enter fifth student marks:"))
+# m6=float(input("Enter sixth student marks:"))
+# marks.append(m1)
+# marks.append(m2)
+# marks.append(m3)
+# marks.append(m4)
+# marks.append(m5)
+# marks.append(m6)
+# print(marks)
+# marks.sort()
+# print(marks)
+
+tuple=("Sushmita","Ranjita","Sammu")
+tuple[1]="SSS"
+
+list=[23,45,67,89,98,103]
+print(sum(list))
+
+a=(7,0,8,0,0,9)
+print(a.count(0))
+
+marks=[78,65,89,45,92]
+total=sum(marks)
+print("The total marks are:",sum(marks))
+print("The highest marks is:",max(marks))
+print("The lowest marks is:",min(marks))
+length=len(marks)
+avg=total/length
+print(avg)
+
+# dictionary : used to store the element in key value pairs
+table = {
+    "name":"Sushmita",
+    "sslc_marks": 95,
+    "school":"JVMK",
+    "university":"AIET",
+}
+print(table)
+print(type(table))
+print(table["name"]) # to get the value of the respective key in the dict
+
+# we can store a list in dict also 
+dict={
+    "name":"Sushmita",
+    "marks":[100,99,20]
+}
+print(dict)
+print(dict["name"])
+print(dict["marks"])
+
+# dictionary methods
+print(dict.items())
+print(dict.keys())
+print(dict.values())
+dict.update({"name":"Ranjita","age":19})
+print(dict)
+print(dict.get("age2")) # prints none
+print(dict["age2"]) #returns an error
+dict.pop("age")
+print(dict)
+dict.popitem()
+print(dict)
+dict2=dict.copy()
+print(dict2)
+
+# sets in python
+# can't contain repeating elem
+set1={}
+set2={1,2,3}
+print(type(set1)) # returns the type i.e dict
+print(type(set2)) # returns type is set
+
+name=set() # to create an empty set
+set={1,2,3,4,5,6,6,7,5,8,9}
+print(set)
+
+# set methods
+set1={23,45,67,"Sussu","Biri",23,45.6}
+set1.add(45.6)
+print(set1)
+set1.remove("Sussu")
+print(set1)
+set1.pop()
+print(set1)
+
+# union and intersection of sets
+set3={12,34,56,65,43,45,78}
+set4={4,45,23,78,39,56,12,34}
+print("The union of set3 qnd set4 is",set3.union(set4))
+print("The intersection of set3 qnd set4 is",set3.intersection(set4))
+print(set3-set4)
+
+set5={1,2,3,4}
+set6={1,2,3}
+print(set6.issubset(set5))
+print(set6.issuperset(set5))
+print(set5.isdisjoint(set6))
 
 # problems
-fruits=[]
-f1=input("Enter one fruit:")
-f2=input("Enter another fruit:")
-f3=input("Enter next fruit:")
-f4=input("Enter next fruit:")
-fruits.append(f1)
-fruits.append(f2)
-fruits.append(f3)
-fruits.append(f4)
-print(f"The final list that has fruits is {fruits}")
-
-marks=[]
-m1=float(input("Enter first student marks:"))
-m2=float(input("Enter second student marks:"))
-m3=float(input("Enter third student marks:"))
-m4=float(input("Enter fourth student marks:"))
-m5=float(input("Enter fifth student marks:"))
-m6=float(input("Enter sixth student marks:"))
-marks.append(m1)
-marks.append(m2)
-marks.append(m3)
-marks.append(m4)
-marks.append(m5)
-marks.append(m6)
-print(marks)
-marks.sort()
-print(marks)
-
-
+dict={
+    "enu":"What",
+    "yaavdu":"which",
+    "yaake":"why"
+}
+mean=input("Enter the word to get the meaning:")
+print(dict[mean])
